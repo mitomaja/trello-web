305c23e86566b7cee3f4bdc45af6bb9ecc817734
quanghuy2923 <mrzxkun@gmail.com>
2024-05-23 16:12:16 +0700

    Dark Mode

---
+++ src/theme.js
@@ -0,0 +1,23 @@
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles';
+import { red, pink, teal, blue } from '@mui/material/colors'
+
+// Create a theme instance.
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: blue
+      }
+    },
+    dark: {
+      palette: {
+        primary: pink,
+        secondary: red
+      }
+    }
+  }
+  // ...other properties
+})
+
+export default theme
