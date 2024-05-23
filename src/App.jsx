9072e0ec3b7f1ffed77be85ede85bb831659d2a1
quanghuy2923 <mrzxkun@gmail.com>
2024-05-23 23:52:28 +0700

    Refactor Project Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
-import { useColorScheme } from '@mui/material/styles'
+import Board from './pages/Boards/_id'
 
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light');
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  )
-}
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <ModeToggle />
-      <div>test</div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <AccessAlarmIcon/>
-      <ThreeDRotation/>
-      <HomeIcon />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[500] }} />
+      <Board/>
     </>
   )
 }
